bdd8839513ead1a6617d1a95b103eeb777d3264d
Amrendra Singh Rathore <82491797+amrendra-rathore@users.noreply.github.com>
2021-04-15 18:56:53 +0530

    Intro to This Keyword

---
+++ Functions 101/thisKeyword.js
@@ -0,0 +1,13 @@
+//TODO : Light Intro to THIS Keyword.
+
+console.log(this);
+
+
+var game = "football";
+
+// function sayName() {
+//     var name = "Armin"
+//     console.log(this);
+// }
+
+// sayName();
